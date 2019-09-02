9d289299cce0136c8f58ffb890de011d2bb5d771
Anonymous <user@rivne.itstep.org>
2019-09-02 21:00:59 +0300

    Addet class Dog

---
+++ 01.Git/js/app.js
@@ -0,0 +1,12 @@
+class Dog{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    ShowDogInfo = () => {
+        console.log("Name => ",this.name,"Age => ",this.age);
+    }
+}
+let Bobik=new Dog("Bobik",3);
+Bobik.ShowDogInfo();
+console.log("hello");
